5caa80284cfb35224e04c1582ff9305658e7d305
pradumnkumar6 <pradumnkumar6@gmail.com>
2024-05-25 20:40:44 +0530

    Add assessment relavant models and django-admin page configs

--- app/sector_assessment/admin.py
+++ app/sector_assessment/admin.py
@@ -1,3 +1,109 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.http.request import HttpRequest
+from django.utils.translation import gettext_lazy as _
 
+from sector_assessment.models import (Question, Choice,
+                                      Assessment, QuestionAttempt,
+                                      AssessmentRun, Result)
 # Register your models here.
+
+
+# Class to generate choice in line or one line
+# if we want muliple lines we use StackedInline
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    fields = ['choice_text', 'choice_image', 'skills']
+    readonly_fields = ['created_by']
+    filter_horizontal = ['skills']
+    extra = 4
+    ordering = ['_order']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        return super().save_model(request, obj, form, change)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    ordering = ['title']
+    list_display = ['title', 'id', 'created_by']
+    search_fields = ['title', 'id', 'question_text']
+    # Add choice creation in-line
+    inlines = [ChoiceInline]
+    readonly_fields = ['created_by']
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['title', 'question_text', 'question_image']
+            },
+        ),
+    ]
+
+    add_fieldsets = [
+        (
+            None,
+            {
+                'fields': ['title', 'question_text', 'question_image']
+            },
+        ),
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        return super().save_model(request, obj, form, change)
+
+
+@admin.register(Assessment)
+class AssessmentAdmin(admin.ModelAdmin):
+    ordering = ['title']
+    search_fields = ['title', 'id']
+    list_display = ['title', 'id', 'created_by']
+    fields = ['title', 'questions', 'allowed_users']
+    filter_horizontal = ['questions', 'allowed_users']
+    readonly_fields = ['created_by']
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            if Assessment.objects.filter(title__iexact=obj.title).count() > 0:
+                raise ValidationError(
+                    _("A sector with the name '%(value)s' already exists."),
+                    params={'value': obj.title},
+                )
+        if not obj.created_by:
+            obj.created_by = request.user
+        return super().save_model(request, obj, form, change)
+
+
+@admin.register(AssessmentRun)
+class AssessmentRunAdmin(admin.ModelAdmin):
+    list_display = ['assessment', 'id', 'user']
+    search_fields = ['assessment', 'id', 'user']
+    readonly_fields = ('id', 'assessment', 'user', 'start_time',
+                       'question_counter', 'time_taken', 'is_finished')
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
+
+
+@admin.register(QuestionAttempt)
+class QuestionAttemptAdmin(admin.ModelAdmin):
+    list_display = ['question', 'id', 'user']
+    search_fields = ['question', 'id', 'user']
+    readonly_fields = ('id', 'question', 'choice', 'user')
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['assessment_run', 'id', 'user']
+    search_fields = ['assessment_run', 'id', 'user']
+    readonly_fields = ('id', 'assessment_run', 'skill_score', 'user')
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
--- app/sector_assessment/models.py
+++ app/sector_assessment/models.py
@@ -1,3 +1,88 @@
-# from django.db import models
+from django.db import models
 
+from core.models import User, Skill
+
+import uuid
+import os
+import datetime
 # Create your models here.
+
+
+def get_image_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join("question_images", filename)
+
+
+class Question(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                   related_name='question_created_by',
+                                   null=True)
+    title = models.CharField(max_length=512)
+    question_text = models.TextField(blank=True)
+    question_image = models.ImageField(upload_to=get_image_file_path,
+                                       blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Choice(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    choice_text = models.CharField(max_length=512, blank=True)
+    choice_image = models.ImageField(upload_to=get_image_file_path,
+                                     blank=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    skills = models.ManyToManyField(Skill)
+
+    class Meta:
+        order_with_respect_to = 'question'
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Assessment(models.Model):
+    title = models.CharField(max_length=512, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                   null=True,
+                                   related_name='assessment_created_by')
+    questions = models.ManyToManyField(Question)
+    start_time = models.DurationField(null=True)
+    max_time = models.DurationField(default=datetime.timedelta(minutes=10))
+    max_questions = models.PositiveSmallIntegerField(default=30)
+    allowed_users = models.ManyToManyField(User, related_name='allowed_users')
+
+    def __str__(self):
+        return self.title
+
+
+class QuestionAttempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question,
+                                 on_delete=models.CASCADE, editable=False)
+    # choice_selected = models.ForeignKey(Choice, on_delete=models.CASCADE,
+    #                                     editable=False,
+    #                                     related_name='question_submitted_choice')
+    choice = models.ForeignKey(Choice,
+                               on_delete=models.CASCADE, editable=False)
+
+
+class AssessmentRun(models.Model):
+    assessment = models.ForeignKey(Assessment,
+                                   on_delete=models.CASCADE, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    start_time = models.DateTimeField(null=True, blank=True,
+                                      editable=False, auto_now_add=True)
+    question_counter = models.PositiveIntegerField(default=0, editable=False)
+    time_taken = models.DurationField(null=True, blank=True, editable=False)
+    is_finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.assessment.title
+
+
+class Result(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    skill_score = models.JSONField(default=None)
+    assessment_run = models.ForeignKey(AssessmentRun, on_delete=models.CASCADE)
